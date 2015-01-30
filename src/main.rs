5abb6e2ee65286381fd8c19565af7c5c31b840d2
Collin Bell <slightlycyborg@gmail.com>
2015-01-29 22:46:19 -0500

    added get request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate hyper
+
 fn main(){
     println!("This is a simple program that harvests user data from reddit");
+    // Create a client.
+    let mut client = Client::new();
+
+    // Creating an outgoing request.
+    let mut res = client.get("http://www.reddit.com/users/SlightlyCyborg/about/.json")
+        // set a header
+        .header(Connection(vec![Close]))
+        // let 'er go!
+        .send();
+    // Read the Response.
+    let body = res.read_to_string().unwrap();
+
+    println!("Response: {}", res);
+
 }
