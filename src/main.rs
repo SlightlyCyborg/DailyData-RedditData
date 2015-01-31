ae757064fdc7879522f90032c8d304d2c9f1a975
Collin Bell <slightlycyborg@gmail.com>
2015-01-30 20:34:57 -0500

    fuck this

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
-extern crate hyper
 
-fn main(){
-    println!("This is a simple program that harvests user data from reddit");
-    // Create a client.
+    
+
+#![allow(unstable)]
+extern crate hyper;
+
+use std::os;
+use std::old_io::stdout;
+use std::old_io::util::copy;
+
+use hyper::Client;
+
+fn main() {
+    let mut url = "http://reddit.com/users/SlightlyCyborg/about/.json";
+
+println!("{}",url);
+
+
     let mut client = Client::new();
 
-    // Creating an outgoing request.
-    let mut res = client.get("http://www.reddit.com/users/SlightlyCyborg/about/.json")
-        // set a header
-        .header(Connection(vec![Close]))
-        // let 'er go!
-        .send();
-    // Read the Response.
-    let body = res.read_to_string().unwrap();
+    let mut res = match client.get(url).send() {
+        Ok(res) => res,
+        Err(err) => panic!("Failed to connect: {:?}", err)
+    };
 
-    println!("Response: {}", res);
+    println!("Response: {}", res.status);
+    println!("Headers:\n{}", res.headers);
+    match copy(&mut res, &mut stdout()) {
+        Ok(..) => (),
+        Err(e) => panic!("Stream failure: {:?}", e)
+    };
 
 }
